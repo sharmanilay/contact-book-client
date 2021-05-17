593779fc718a2e01af90c083f41dfaa3c294e28d
sharmanilay <nilaysharma642@gmail.com>
2021-05-17 19:54:21 +0530

    cors test

--- src/components/utils/http.js
+++ src/components/utils/http.js
@@ -1,7 +1,5 @@
 import axios from 'axios'
 
-console.log(process.env.REACT_APP_API_BASE_URL)
-
 const http = axios.create({
 	baseURL: process.env.REACT_APP_API_BASE_URL
 })
@@ -9,6 +7,9 @@ const http = axios.create({
 http.interceptors.request.use(function (config) {
 	const token = localStorage.getItem('default_auth_token')
 	config.headers.Authorization = token ? `Bearer ${token}` : ''
+	config.headers[
+		'Access-Control-Allow-Origin'
+	] = `${process.env.REACT_APP_ORIGIN_URL}`
 	return config
 })
 
